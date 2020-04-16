4bbaf546d5b16b552b6e5ada9fcf261a260d0e1f
Subilan <suyuanliang1995@gmail.com>
2020-04-16 15:41:57 +0800

    Import vue-material

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import router from './router'
 import "@/css/mono.less"
 import '@mdi/font/css/materialdesignicons.min.css'
 import Server from '@/server';
+import "vue-material/dist/vue-material.min.css";
+import "./css/default.css";
+import "./css/override.less";
 
 Vue.config.productionTip = false
 Vue.prototype.$server = Server;
