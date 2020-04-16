d8e10aefd3ee69f260dbde8cafee6d518ee37900
Subilan <suyuanliang1995@gmail.com>
2020-04-16 15:42:16 +0800

    Add admin route n scroll behavior for router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,44 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import("@/views/Home.vue")
-  },
-]
+	{
+		path: "/",
+		name: "home",
+		component: () => import("@/views/Home.vue")
+	},
+	{
+		path: "/admin",
+		name: "admin",
+		component: () => import("@/views/Admin.vue"),
+		children: [
+			{
+				path: "new",
+				name: "admin-new",
+				component: () => import("@/views/AdminNew.vue")
+			}
+		]
+	}
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+	mode: "history",
+	scrollBehavior (to, from, spos) {
+		if (to.hash) {
+			return {
+				selector: to.hash
+			}
+		} else {
+			return {
+				x: 0,
+				y: 0,
+			}
+		}
+	},
+	base: process.env.BASE_URL,
+	routes
+});
 
-export default router
+export default router;
