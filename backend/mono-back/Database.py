d10139b8f0aaf69f919cd288d45473ba7d2732b1
Subilan <suyuanliang1995@gmail.com>
2020-04-18 20:21:27 +0800

    Move functions to Logue.py

--- backend/mono-back/Database.py
+++ backend/mono-back/Database.py
@@ -1,49 +1,29 @@
 import pymysql;
-import time;
-from flask import jsonify
 
 class DBController:
-    def getInstance(self):
-        return pymysql.connect(
+    def __init__(self):
+        self.instance = pymysql.connect(
             host = "localhost",
             user = "root",
             password = "subilan1999",
             database = "Monologue",
             charset = "utf8mb4"
         )
+        super().__init__()
+    
+    def cursor(self, dict = False):
+        if (dict):
+            return self.instance.cursor(pymysql.cursors.DictCursor)
+        return self.instance.cursor()
 
-    def getLogues(self, data_range = "0,10"):
-        db = self.getInstance()
-        cur = db.cursor()
-        sql = ("SELECT * FROM Logue LIMIT " + data_range)
-        try:
-            cur.execute(sql)
-            db.commit()
-            data = cur.fetchall()
-        except Exception:
-            print(Exception)
-            self.connect.rollback()
-            return False
-        finally:
-            db.close()
-            cur.close()
-        return jsonify(data)
+    def commit(self):
+        self.instance.commit()
+        return
 
-    def writeLogue(self, title, contents):
-        db = self.getInstance()
-        cur = db.cursor()
-        extdate = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        date = time.strftime("%Y-%m-%d", time.localtime())
-        sql = ("INSERT INTO Logue (title, contents, date, extdate) VALUES ('{t}', '{c}', '{d}', '{ed}')")
-        sql = sql.format(t = title, c = contents, d = date, ed = extdate)
-        try:
-            cur.execute(sql)
-            db.commit()
-        except Exception:
-            print(Exception)
-            self.connect.rollback()
-            return False
-        finally:
-            cur.close()
-            db.close()
-        return True
+    def close(self):
+        self.instance.close()
+        return
+
+    def rollback(self):
+        self.instance.rollback()
+        return
