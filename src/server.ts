76ad55b1080fc31f2782f3cefed05023845051ec
Subilan <suyuanliang1995@gmail.com>
2020-04-18 20:22:26 +0800

    Server class typescript support

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
-import qs from 'qs';
 import axios from 'axios';
 
-class Server {
+declare module "vue/types/vue" {
+	interface Vue {
+		$server: ServerController;
+	}
+}
+
+interface ServerController {
+	post(url: string, data: object, callback: Function): void
+}
+
+const Server: ServerController = class {
     public static post(url: string, data: object, callback: Function) {
-        axios.post(url, qs.stringify(data)).then(r => {
+        axios.post(url, JSON.stringify(data)).then(r => {
             callback(r);
         }).catch(r => {
             console.error(r);
