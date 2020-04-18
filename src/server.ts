c6e9998d68081fa84ea845dffb7dbd82a84a4455
Subilan <suyuanliang1995@gmail.com>
2020-04-18 16:13:34 +0800

    Move frontend files to /

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import qs from 'qs';
 import axios from 'axios';
 
 class Server {
-    public static post(data: object, callback: Function) {
-        axios.post("/api/data.php", qs.stringify(data)).then(r => {
+    public static post(url: string, data: object, callback: Function) {
+        axios.post(url, qs.stringify(data)).then(r => {
             callback(r);
         }).catch(r => {
             console.error(r);
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -7,6 +7,15 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-    
+    data() {
+
+    },
+    mounted() {
+        this.$server.post({
+            method: "auth"
+        }, r => {
+            console.log(r.data);
+        })
+    }
 })
 </script>
