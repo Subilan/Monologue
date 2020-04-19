e04531478cad2bb241e9c10b55ca4e4bf1f65b76
Subilan <suyuanliang1995@gmail.com>
2020-04-19 11:56:26 +0800

    Interfaces

---
+++ src/interface.d.ts
@@ -0,0 +1,23 @@
+interface StringMatch {
+	[props: string]: string;
+}
+
+interface MatchObject {
+	[props: string]: StringMatch;
+}
+
+interface LogueArrayItem {
+	date: string;
+	time: string;
+	logue: Array<Logue>;
+}
+
+interface Logue {
+	title: string;
+	type: string;
+	contents: string;
+}
+
+interface LogueOrigin extends Logue {
+	date: string;
+}
