d14014b049cd38b2e20a5cc1d5a7025c166b6381
Subilan <suyuanliang1995@gmail.com>
2020-04-22 12:09:36 +0800

    Edit functions in the same file

--- src/views/AdminNewEvent.vue
+++ src/views/AdminNewEvent.vue
@@ -1,39 +1,47 @@
 <template>
-  <div class="admin-new-event admin-container">
-    <div class="hero">
-      <h1>添加新事件</h1>
-      <p>在时间线上添加新的事件以供外部参考。</p>
-    </div>
-    <div class="new-event-form">
-      <md-field :class="titleInvalid">
-        <md-icon class="mdi mdi-format-title" />
-        <label>事件标题</label>
-        <md-input type="text" v-model="title" maxlength="40" required />
-        <span class="md-helper-text">不能超过 40 字。</span>
-        <span class="md-error">长度无效</span>
-      </md-field>
-      <md-field class="textarea" :class="contentInvalid">
-        <label>事件内容</label>
-        <md-textarea type="text" v-model="content" maxlength="1000" required />
-        <span class="md-helper-text">支持 Markdown 语法，不能超过 1000 字。</span>
-        <span class="md-error">长度无效</span>
-      </md-field>
-      <md-field>
-        <md-icon class="mdi" :class="getIconByType()" />
-        <label>选择事件类型</label>
-        <md-select required v-model="type">
-          <md-option value="info">一般</md-option>
-          <md-option value="warning">警告</md-option>
-          <md-option value="solved">已解决</md-option>
-        </md-select>
-      </md-field>
-      <md-button @click="submit()" class="submit-btn md-primary md-raised">提交</md-button>
-      <md-snackbar
-        :md-active.sync="snackbar"
-        md-position="center"
-        :md-duration="3000"
-        md-persistent
-      >{{ snackbarMessage }}</md-snackbar>
+  <div class="admin-event admin-container">
+    <md-empty-state v-if="invalidID">
+      <span class="md-empty-state-icon mdi mdi-help-circle-outline"/>
+      <span class="md-empty-state-label">无效的 ID</span>
+      <span class="md-empty-state-description">您输入的 ID 有误或者不存在</span>
+      <md-button @click="$router.go(-1)" class="md-primary md-raised">后退</md-button>
+    </md-empty-state>
+    <div class="full-height" v-if="!isEdit() || !invalidID">
+      <div class="hero">
+        <h1>添加新事件</h1>
+        <p>在时间线上添加新的事件以供外部参考。</p>
+      </div>
+      <div class="new-event-form full-height">
+        <md-field :class="titleInvalid">
+          <md-icon class="mdi mdi-format-title" />
+          <label>事件标题</label>
+          <md-input type="text" v-model="title" maxlength="40" required />
+          <span class="md-helper-text">不能超过 40 字。</span>
+          <span class="md-error">长度无效</span>
+        </md-field>
+        <md-field class="textarea" :class="contentInvalid">
+          <label>事件内容</label>
+          <md-textarea type="text" v-model="content" maxlength="1000" required />
+          <span class="md-helper-text">支持 Markdown 语法，不能超过 1000 字。</span>
+          <span class="md-error">长度无效</span>
+        </md-field>
+        <md-field>
+          <md-icon class="mdi" :class="getIconByType()" />
+          <label>选择事件类型</label>
+          <md-select required v-model="type">
+            <md-option value="info">一般</md-option>
+            <md-option value="warning">警告</md-option>
+            <md-option value="solved">已解决</md-option>
+          </md-select>
+        </md-field>
+        <md-button @click="submit()" class="submit-btn md-primary md-raised">提交</md-button>
+        <md-snackbar
+          :md-active.sync="snackbar"
+          md-position="center"
+          :md-duration="3000"
+          md-persistent
+        >{{ snackbarMessage }}</md-snackbar>
+      </div>
     </div>
   </div>
 </template>
@@ -52,13 +60,16 @@ import MdMenu from "vue-material/dist/components/MdMenu";
 import MdList from "vue-material/dist/components/MdList";
 // @ts-ignore
 import MdSnackbar from "vue-material/dist/components/MdSnackbar";
+// @ts-ignore
+import MdEmptyState from "vue-material/dist/components/MdEmptyState";
 
 Vue.use(MdField)
   .use(MdIcon)
   .use(MdButton)
   .use(MdMenu)
   .use(MdList)
-  .use(MdSnackbar);
+  .use(MdSnackbar)
+  .use(MdEmptyState);
 
 export default Vue.extend({
   data() {
@@ -66,11 +77,13 @@ export default Vue.extend({
       title: "",
       content: "",
       type: "info",
+      id: -1,
       titleInvalid: "",
       contentInvalid: "",
       snackbar: false,
       snackbarMessage: "",
-      disableSubmit: false
+      disableSubmit: false,
+      invalidID: false,
     };
   },
   watch: {
@@ -98,6 +111,9 @@ export default Vue.extend({
       };
       return match[this.type];
     },
+    isEdit() {
+      return this.$route.name === "admin-edit-event";
+    },
     submit() {
       if (this.disableSubmit) {
         return false;
@@ -105,9 +121,11 @@ export default Vue.extend({
       this.$server.post(
         "/api/logue",
         {
+          method: this.isEdit() ? "alter" : "submit",
           title: this.title,
           contents: this.content,
-          type: this.type
+          type: this.type,
+          id: this.id
         },
         r => {
           if (r.data) {
@@ -126,15 +144,27 @@ export default Vue.extend({
         }
       );
     }
+  },
+  mounted() {
+    if (this.isEdit()) {
+      this.id = Number(this.$route.params.id);
+      if (this.id > 0 && Number.isInteger(this.id) && this.id !== NaN) {
+        this.$server.get("/api/logue?method=id&markdown=false&id=" + this.id, r => {
+          let data = r.data;
+          this.title = data.title;
+          this.content = data.contents;
+          this.type = data.type;
+        });
+      } else {
+        this.invalidID = true;
+      }
+    }
   }
 });
 </script>
 
 <style lang="less" scoped>
 .new-event-form {
-  height: 100%;
-  position: relative;
-
   .textarea {
     position: relative;
     height: 50% !important;
