09decc60714bf93890dde70c146472b6bed98aaa
Subilan <suyuanliang1995@gmail.com>
2020-04-25 18:32:29 +0800

    PWA

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     devServer: {
         proxy: "http://localhost:5000"
+    },
+    pwa: {
+        name: "Monologue",
+        themeColor: "#000",
+        msTileColor: "#000",
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        workboxPluginMode: "GenerateSW"
     }
 }
