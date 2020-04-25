21e3dbf68ab81568b4a9eb9f31295ff0bc69377a
Subilan <suyuanliang1995@gmail.com>
2020-04-26 00:53:58 +0800

    isPC function (please use CSS @media rules first)

---
+++ src/functions.ts
@@ -0,0 +1,9 @@
+export function isPC(): boolean {  
+    var userAgentInfo = navigator.userAgent;
+    var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");  
+    var flag = true;  
+    for (var v = 0; v < Agents.length; v++) {  
+        if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }  
+    }  
+    return flag;
+ }
