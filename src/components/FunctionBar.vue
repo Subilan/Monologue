559896bd6cf08811b5497ba81416e48cd0bb5837
Subilan <suyuanliang1995@gmail.com>
2020-04-25 17:53:40 +0800

    +Component FunctionBar

---
+++ src/components/FunctionBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="functions">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
+
+<style lang="less" scoped>
+.functions {
+  position: absolute;
+  top: 32px;
+  right: 0;
+  display: flex;
+  align-items: center;
+
+  * {
+    margin-left: 8px;
+    margin-right: 8px;
+
+    &:first-child {
+      margin-left: 0;
+    }
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+}
+</style>
