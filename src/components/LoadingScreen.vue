1268c33e1ddba755678aa641fb6a79b50bbab6cc
Subilan <suyuanliang1995@gmail.com>
2020-04-25 17:53:29 +0800

    +Component LoadingScreen

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="loading-screen">
+      <div class="loading">
+        <md-progress-spinner md-mode="indeterminate"/>
+      </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+// @ts-ignore
+import MdProgress from 'vue-material/dist/components/MdProgress';
+
+Vue.use(MdProgress);
+
+export default Vue.extend({
+    
+})
+</script>
