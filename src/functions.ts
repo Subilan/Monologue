0e571c93c81d7494373f8552a9180fa4080663cb
Subilan <suyuanliang1995@gmail.com>
2020-04-27 19:19:14 +0800

    Copy function with fallback

--- src/functions.ts
+++ src/functions.ts
@@ -7,3 +7,33 @@ export function isPC(): boolean {
     }  
     return flag;
  }
+
+ // do not use the function in return or if statements because there is something async which is impossible to return a value in a sync way.
+ export function copy(text: string, callback: Function | undefined = undefined): boolean {
+     if (!navigator.clipboard) {
+         let a = document.createElement("textarea");
+         a.value = text;
+         a.style.top = "0";
+         a.style.left = "0";
+         a.style.position = "fixed";
+
+         document.body.appendChild(a);
+         a.focus();
+         a.select();
+         try {
+             return document.execCommand("copy");
+         } catch (e) {
+             return false;
+         }
+         document.body.removeChild(a);
+     } else {
+         navigator.clipboard.writeText(text).then(r => {
+             if (callback) {
+                callback();
+             }
+         }, err => {
+             console.error(err);
+         });
+         return true;
+     }
+ }
