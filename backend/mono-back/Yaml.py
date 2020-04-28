e2f3649ff7f181d20098044621c5affb3a574b3c
Subilan <suyuanliang1995@gmail.com>
2020-04-28 14:06:08 +0800

    Python 2.7 compatibility

--- backend/mono-back/Yaml.py
+++ backend/mono-back/Yaml.py
@@ -1,11 +1,12 @@
 import yaml
 import os
+import io
 
 class Config:
     def __init__(self):
         path = os.path.dirname(os.path.realpath(__file__))
         yamlPath = os.path.join(path, "config.yml")
-        self.raw = open(yamlPath, 'r', encoding="utf-8").read()
+        self.raw = io.open(yamlPath, 'r', encoding="utf-8").read()
         self.cfg = yaml.load(self.raw)
         pass
 
