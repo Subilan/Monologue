8d19c4cc7b4136645dbf9a9c97b23273aa5e52d7
Subilan <suyuanliang1995@gmail.com>
2020-04-30 22:59:05 +0800

    New component InfoBar

--- src/components/InfoBar.vue
+++ src/components/InfoBar.vue
@@ -1,36 +1,88 @@
 <template>
-    <div class="information-bar">
-
-    </div>
+	<div class="information-bar" :class="[color]" :style="{opacity}">
+		<slot></slot>
+	</div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 // @ts-ignore
-import MdIcon from 'vue-material/dist/components/MdIcon';
+import MdIcon from "vue-material/dist/components/MdIcon";
 
 Vue.use(MdIcon);
 
 export default Vue.extend({
-    
-})
+	props: ["color"],
+	data() {
+		return {
+			opacity: 1,
+		}
+	},
+	mounted() {
+		let lastScrollTop = 0;
+		let st;
+		window.onscroll = e => {
+			st = window.pageYOffset || document.documentElement.scrollTop;
+			if (st > lastScrollTop) {
+				this.opacity = 0;
+			} else {
+				this.opacity = 1;
+			}
+			lastScrollTop = st <= 0 ? 0 : st;
+		}
+	}
+});
 </script>
 
 <style lang="less" scoped>
 .information-bar {
 	&.blue {
-		background-color: @blue-light;
-		color: @blue-primary;
+		&,
+		.mdi::before {
+			background-color: @blue-light;
+			color: @blue-primary;
+		}
 	}
 
 	&.red {
-		background-color: @red-light;
-		color: @red-primary;
+		&,
+		.mdi::before {
+			background-color: @red-light;
+			color: @red-primary;
+		}
 	}
 
 	&.green {
-		background-color: @green-light;
-		color: @green-primary;
+		&,
+		.mdi::before {
+			background-color: @green-light;
+			color: @green-primary;
+		}
 	}
+
+	.mdi {
+		&:first-child {
+			margin-right: 16px;
+		}
+
+		&:last-child {
+			margin-left: 16px;
+		}
+	}
+
+	.fontfamily-default;
+	z-index: 1000;
+	font-weight: 500;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: fixed;
+	font-size: 18px;
+	top: 0;
+	left: 0;
+	height: 48px;
+	transition: opacity .2s ease;
+	cursor: pointer;
 }
 </style>
