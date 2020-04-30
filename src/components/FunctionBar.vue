407c05f945d6c22969e230feb2daf1773686ed3b
Subilan <suyuanliang1995@gmail.com>
2020-04-30 09:07:41 +0800

    Optional single item display and move rules to global less file

--- src/components/FunctionBar.vue
+++ src/components/FunctionBar.vue
@@ -1,22 +1,20 @@
 <template>
-    <div>
-        <div v-if="isPC() || singleOnMobile" class="functions">
-            <slot></slot>
-        </div>
+	<div>
+		<div v-if="pc || singleOnMobile" class="functions">
+			<div class="items">
+				<slot></slot>
+			</div>
+		</div>
 
-        <md-menu
-            v-if="!isPC() && !singleOnMobile"
-            md-direction="bottom-start"
-            class="mobile-menu-btn"
-        >
-            <md-button md-menu-trigger class="md-raised md-icon-button">
-                <md-icon class="mdi mdi-plus-circle" />
-            </md-button>
-            <md-menu-content class="mobile-menu">
-                <slot></slot>
-            </md-menu-content>
-        </md-menu>
-    </div>
+		<md-menu :class="$route.name === 'home' ? 'home-scoped' : ''" v-if="!pc && !singleOnMobile" md-direction="bottom-start" class="mobile-menu-btn">
+			<md-button md-menu-trigger class="md-raised md-icon-button">
+				<md-icon class="mdi mdi-plus-circle" />
+			</md-button>
+			<md-menu-content class="mobile-menu">
+				<slot></slot>
+			</md-menu-content>
+		</md-menu>
+	</div>
 </template>
 
 <script lang="ts">
@@ -32,71 +30,45 @@ import MdButton from "vue-material/dist/components/MdButton";
 import { isPC } from "@/functions";
 
 Vue.use(MdMenu)
-    .use(MdList)
-    .use(MdIcon)
-    .use(MdButton);
+	.use(MdList)
+	.use(MdIcon)
+	.use(MdButton);
 
 export default Vue.extend({
-    data() {
-        return {
-            singleOnMobile: false
-        };
-    },
-    methods: {
-        isPC
-    },
-    mounted() {
-        if (
-            //@ts-ignore
-            (this.$slots.default.length === 2 &&
-                //@ts-ignore
-                this.$slots.default[0].tag === undefined) ||
-            //@ts-ignore
-            this.$slots.default.length === 1
-        ) {
-            this.singleOnMobile = true;
-        }
-    }
+    props: ["ignoreSingle"],
+	data() {
+		return {
+			singleOnMobile: false,
+			pc: false,
+		};
+	},
+	methods: {
+		isPC
+	},
+	mounted() {
+		let ignoreSingle = this.ignoreSingle === true ? true : false;
+		// @ts-ignore
+		if (((this.$slots.default.length === 2 && this.$slots.default[0].tag === undefined) || this.$slots.default.length === 1) && ignoreSingle === false) {
+			this.singleOnMobile = true;
+		}
+		this.pc = isPC()
+	}
 });
 </script>
 
 <style lang="less" scoped>
-.functions {
-    @media screen and (max-width: 1024px) {
-        top: -16px;
-    }
-
-    @media screen and (min-width: 1024px) {
-        top: 32px;
-    }
-
-    position: absolute;
-    right: 0;
-    display: flex;
-    align-items: center;
-
-    * {
-        margin-left: 8px;
-        margin-right: 8px;
-
-        &:first-child {
-            margin-left: 0;
-        }
-
-        &:last-child {
-            margin-right: 0;
-        }
-    }
-}
-
 .mobile-menu-btn {
-    position: absolute;
-    top: -16px;
+	position: absolute;
+	top: -16px;
     right: 0;
+
+    &.home-scoped {
+        top: 16px !important;
+    }
 }
 
 .mobile-menu {
-    display: flex;
-    align-items: center;
+	display: flex;
+	align-items: center;
 }
 </style>
