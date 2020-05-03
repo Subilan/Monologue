e002687e84accd2ceec3fff739e87f5cc551aa8e
Subilan <suyuanliang1995@gmail.com>
2020-05-03 17:47:18 +0800

    Fix depracted usage of yaml.load (use yaml.FullLoader as default loader)

--- backend/mono-back/Yaml.py
+++ backend/mono-back/Yaml.py
@@ -7,7 +7,7 @@ class Config:
         path = os.path.dirname(os.path.realpath(__file__))
         yamlPath = os.path.join(path, "config.yml")
         self.raw = io.open(yamlPath, 'r', encoding="utf-8").read()
-        self.cfg = yaml.load(self.raw)
+        self.cfg = yaml.load(self.raw, Loader=yaml.FullLoader)
         pass
 
     def getConfig(self, key = None):
