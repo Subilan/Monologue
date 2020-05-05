be333e45eb9b51bb193f95961709ce31061255ff
Subilan <suyuanliang1995@gmail.com>
2020-05-06 00:51:44 +0800

    Agreement table struct

--- backend/mono-back/database.sql
+++ backend/mono-back/database.sql
@@ -14,3 +14,13 @@ CREATE TABLE IF NOT EXISTS `User` (
     `password` varchar(512) NOT NULL,
     PRIMARY KEY (`id`)
 ) CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `Agreement` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `title` varchar(50) NOT NULL,
+    `contents` text NOT NULL,
+    `disagreement` VARCHAR(10) NOT NULL,
+    `approvers` TEXT,
+    `opponents` TEXT,
+    `date` varchar(20) NOT NULL,
+    PRIMARY KEY (`id`)
+) CHARSET=utf8mb4;
