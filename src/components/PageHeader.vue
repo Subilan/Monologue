2ada6f729e546cece0a5e1e5c778c063c815541b
Subilan <suyuanliang1995@gmail.com>
2020-05-08 21:37:02 +0800

    Create new component PageHeader

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="header">
+		<div class="hero">
+			<slot />
+		</div>
+		<function-bar class="function-bar-float-right">
+			<slot name="functions" />
+		</function-bar>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import FunctionBar from "@/components/FunctionBar.vue";
+
+export default Vue.extend({
+	components: {
+		FunctionBar
+	}
+});
+</script>
